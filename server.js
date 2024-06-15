6b4a4743f4db9641ad08957b4761763037216dd4
lillyasquith <asq22001@byui.edu>
2024-06-14 23:19:42 -0600

    OAuth setup

--- server.js
+++ server.js
@@ -2,23 +2,45 @@ const express = require( 'express' );
 const app = express();
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
+const passport = require('passport');
+const session = require ('express-session');
+const cors = require ('cors');
+const gitHub = require('passport-github2').Strategy;
 
 const port = process.env.PORT || 3001;
 
-app.use(bodyParser.json());
+app
+  .use(bodyParser.json())
+  .use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  })
+  .use(cors({methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']}))
+  .use(cors({origin: '*'}))
+  .use('/', require('./routes'));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-  'Access-Control-Allow-Headers',
-  'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-});
+  passport.use(new gitHub({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
 
-
-app.use('/', require('./routes'));
 
 process.on('uncaughtException', (err, origin) => {
   console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
@@ -33,6 +55,5 @@ mongodb.initDb((err) => {
       app.listen(port, () => {
         console.log(`Running on port ${port}`);
       });
-    }
-  
+    } 
 });
